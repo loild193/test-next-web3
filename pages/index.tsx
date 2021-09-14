483f517c091ad14da87692b80e013925c9ee7ccc
Le Duc Loi <53306165+LeDucLoi193@users.noreply.github.com>
2021-09-14 10:35:59 +0700

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,61 @@
+import { useWeb3React } from "@web3-react/core";
+import Head from "next/head";
+import Link from "next/link";
+import Account from "../components/Account";
+import ETHBalance from "../components/ETHBalance";
+import useEagerConnect from "../hooks/useEagerConnect";
+
+function Home() {
+  const { account, library } = useWeb3React();
+
+  const triedToEagerConnect = useEagerConnect();
+
+  const isConnected = typeof account === "string" && !!library;
+
+  return (
+    <div>
+      <Head>
+        <title>next-web3-boilerplate</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>
+        <nav>
+          <Link href="/">
+            <a>next-web3-boilerplate</a>
+          </Link>
+
+          <Account triedToEagerConnect={triedToEagerConnect} />
+        </nav>
+      </header>
+
+      <main>
+        <h1>
+          Welcome to{" "}
+          <a href="https://github.com/mirshko/next-web3-boilerplate">
+            next-web3-boilerplate
+          </a>
+        </h1>
+
+        {isConnected && (
+          <section>
+            <ETHBalance />
+          </section>
+        )}
+      </main>
+
+      <style jsx>{`
+        nav {
+          display: flex;
+          justify-content: space-between;
+        }
+
+        main {
+          text-align: center;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default Home;
